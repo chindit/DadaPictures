92416d7b4cf5a96798d6d4bd8534e32b3ad38bc3
chindit <littletiger58@gmail.com>
2017-08-03 21:26:58 +0200

    Rand

--- src/AppBundle/Repository/PictureRepository.php
+++ src/AppBundle/Repository/PictureRepository.php
@@ -32,7 +32,7 @@ class PictureRepository extends \Doctrine\ORM\EntityRepository
             ->where('t.id IS NULL')
             ->andWhere('p.status = :status')
             ->setParameter('status', Status::OK)
-            ->orderBy('p.id', 'rand')
+            ->orderBy('RAND()')
             ->setMaxResults(1)
             ->getQuery()
             ->getSingleResult();
