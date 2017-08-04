2e1c8d93b4d4d3e3471137d0033f5dea09ecae4c
David <david@likebook.me>
2017-08-04 18:43:54 +0200

    Added FTP upload capabilities

---
+++ src/AppBundle/Controller/FileController.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Pack;
+use AppBundle\Service\UploadManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class FileController
+ * @package AppBundle\Controller
+ * @Route("file")
+ */
+class FileController extends Controller
+{
+    /**
+     * List current packs in temporary directory
+     *
+     * @Route("/new/{name}", name="file_list", defaults={"name": ""})
+     * @Method("GET")
+     */
+    public function newAction(string $name = ""): Response
+    {
+        $tmpDir = $this->getParameter('ftp_dir');
+        // Read files from first level in temp dir
+        $detectedFiles = (is_dir($tmpDir)) ? scandir($tmpDir) : [];
+
+        if (!empty($name)) {
+            if (!in_array($name, $detectedFiles)) {
+                $this->get('session')->getFlashBag()->add('danger', 'Pack not found');
+            } else {
+                $pack = new Pack();
+                $pack->setCreator($this->getUser());
+                $pack->setStoragePath($tmpDir . '/');
+                $file = new File($tmpDir . '/' . $name);
+                $pack->setFile($file);
+                $uploadManager = $this->get(UploadManager::class);
+                $pack = $uploadManager->upload($pack);
+
+                return $this->redirectToRoute('pack_pre_show', array('id' => $pack->getId()));
+            }
+        }
+
+        $packs = [];
+        foreach ($detectedFiles as $file) {
+            if ($file === '.' || $file === '..') {
+                continue;
+            }
+            $pack = [];
+            $pack['name'] = $file;
+            $pack['size'] = filesize($tmpDir . '/' . $file) / 1024 / 1024; // Mo
+            $packs[] = $pack;
+        }
+
+        return $this->render(':file:index.html.twig', ['packs' => $packs]);
+    }
+}
