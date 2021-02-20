d9f3b9d5ea8457f81ba77284a4f7fd63a2bd3a05
chindit <littletiger58@gmail.com>
2021-02-20 16:01:12 +0100

    Crop picture before creating a thumbnail

--- src/Service/PictureConverter.php
+++ src/Service/PictureConverter.php
@@ -39,22 +39,27 @@ final class PictureConverter
     {
         $picturePath = $this->path->getPictureFullpath($picture);
         $image = ImageFactory::getResource($picturePath);
-            $thumb = imagecreatetruecolor($this->thumbnailWidth, $this->thumbnailHeight);
-            if ($thumb === false) {
+        $thumb = imagecreatetruecolor($this->thumbnailWidth, $this->thumbnailHeight);
+        if ($thumb === false) {
             throw new \Exception(sprintf('Unable to create thumbnail for picture %s', $picture->getFilename()));
-            }
-            imagecopyresized(
-                $thumb,
-                $image,
-                0,
-                0,
-                0,
-                0,
-                $this->thumbnailWidth,
-                $this->thumbnailHeight,
-                $picture->getWidth() ?? 0,
-                $picture->getHeight() ?? 0
-            );
+        }
+
+        $ratio = floor(max([($picture->getWidth() ?? 0) / $this->thumbnailWidth, ($picture->getHeight() ?? 0) / $this->thumbnailHeight]));
+
+		$image = imagecrop($image, ['x' => 0, 'y' => 0, 'width' => $this->thumbnailWidth * $ratio, 'height' => $this->thumbnailHeight * $ratio]);
+
+        imagecopyresized(
+            $thumb,
+            $image,
+            0,
+            0,
+            0,
+            0,
+            $this->thumbnailWidth,
+            $this->thumbnailHeight,
+            $picture->getWidth() ?? 0,
+            $picture->getHeight() ?? 0
+        );
 
         $name = uniqid('thumb_', true) . '.jpg';
         imagejpeg($thumb, $this->path->getThumbnailsDirectory() . $name);
