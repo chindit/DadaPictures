f048da4a0bea2d6d5df16531b1c5935760072f23
chindit <littletiger58@gmail.com>
2022-04-01 20:32:28 +0200

    Create tag update migration

---
+++ src/Migrations/Version20220401144737.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220401144737 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Update Tag name and visibility due to new «visible» field';
+    }
+
+    public function up(Schema $schema): void
+    {
+		$this->addSql(<<<'SQL'
+            UPDATE tag t
+            INNER JOIN translated_tag tt ON t.id = tt.tag_id
+            SET t.name =
+            LOWER(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(
+                TRIM(
+                    tt.name
+                ), ':', ''), ')', ''), '(', ''), ',', ''), '\\', ''), '\/', ''), '\"', ''), '?', ''),
+                '\'', ''), '&', ''), '!', ''), '.', ''), ' ', '-'), '--', '-'), '--', '-'));
+            SQL
+		);
+		$this->addSql('UPDATE tag SET visible=1');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // Nothing to do here
+    }
+}
